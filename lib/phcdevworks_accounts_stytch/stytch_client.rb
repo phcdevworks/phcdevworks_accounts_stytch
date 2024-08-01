f9b64f02316ef12600e9dcf0a9d2eb6005e8e572
Bradley Potts <brad.potts@coastdigitalgroup.com>
2024-08-01 11:24:56 +0800

    added error handling in stytch client

--- lib/phcdevworks_accounts_stytch/stytch_client.rb
+++ lib/phcdevworks_accounts_stytch/stytch_client.rb
@@ -1,13 +1,16 @@
-# frozen_string_literal: true
-
-require 'stytch'
+# lib/phcdevworks_accounts_stytch/stytch_client.rb
 
 module PhcdevworksAccountsStytch
   class StytchClient
     def self.client
+      project_id = Rails.application.credentials.dig(:stytch, :project_id)
+      secret = Rails.application.credentials.dig(:stytch, :secret)
+
+      raise 'Stytch credentials are missing' unless project_id && secret
+
       @client ||= StytchB2B::Client.new(
-        project_id: Rails.application.credentials.dig(:stytch, :project_id),
-        secret: Rails.application.credentials.dig(:stytch, :secret)
+        project_id: project_id,
+        secret: secret
       )
     end
   end
