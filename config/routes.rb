9c3a5f3e46e787b4061c7ba23d55a88d9c783c1d
BradPotts <brad.potts@coastdigitalgroup.com>
2024-09-04 11:44:21 +0800

    completed search for orgid by slug in url

--- config/routes.rb
+++ config/routes.rb
@@ -3,30 +3,31 @@
 PhcdevworksAccountsStytch::Engine.routes.draw do
   namespace :b2b do
     # GET routes for displaying forms or views
-    get 'magic_links/invite/:organization_slug', to: 'magic_links#invite', as: 'invite'
-    get 'magic_links/authenticate/:organization_slug', to: 'magic_links#authenticate', as: 'authenticate'
-    get 'magic_links/login/:organization_slug', to: 'magic_links#login_or_signup', as: 'login_or_signup'
-    get 'magic_links/signup/:organization_slug', to: 'magic_links#login_or_signup', as: 'signup'
+    get 'magic_links/invite/:organization_slug', to: 'magic_links#invite', as: 'magic_links_invite'
+    get 'magic_links/authenticate/:organization_slug', to: 'magic_links#authenticate', as: 'magic_links_authenticate'
+    get 'magic_links/login/:organization_slug', to: 'magic_links#login_or_signup', as: 'magic_links_login'
+    get 'magic_links/signup/:organization_slug', to: 'magic_links#login_or_signup', as: 'magic_links_signup'
 
     # POST routes for processing form submissions or API requests
     post 'magic_links/process_authenticate/:organization_slug', to: 'magic_links#process_authenticate',
-                                                                as: 'process_authenticate'
-    post 'magic_links/process_invite/:organization_slug', to: 'magic_links#process_invite', as: 'process_invite'
+                                                                as: 'magic_links_process_authenticate'
+    post 'magic_links/process_invite/:organization_slug', to: 'magic_links#process_invite', as: 'magic_links_process_invite'
     post 'magic_links/process_login_or_signup/:organization_slug', to: 'magic_links#process_login_or_signup',
-                                                                   as: 'process_login_or_signup'
+                                                                   as: 'magic_links_process_login_or_signup'
   end
 
   namespace :b2c do
     # GET routes for displaying forms or views
-    get 'magic_links/invite', to: 'magic_links#invite'
-    get 'magic_links/authenticate', to: 'magic_links#authenticate'
-    get 'magic_links/login', to: 'magic_links#login_or_signup'
-    get 'magic_links/signup', to: 'magic_links#login_or_signup'
+    get 'magic_links/invite', to: 'magic_links#invite', as: 'magic_links_invite'
+    get 'magic_links/authenticate', to: 'magic_links#authenticate', as: 'magic_links_authenticate'
+    get 'magic_links/login', to: 'magic_links#login_or_signup', as: 'magic_links_login'
+    get 'magic_links/signup', to: 'magic_links#login_or_signup', as: 'magic_links_signup'
 
     # POST routes for processing form submissions or API requests
-    post 'magic_links/process_authenticate', to: 'magic_links#process_authenticate'
-    post 'magic_links/process_invite', to: 'magic_links#process_invite'
-    post 'magic_links/process_revoke_invite', to: 'magic_links#process_revoke_invite'
-    post 'magic_links/process_login_or_signup', to: 'magic_links#process_login_or_signup'
+    post 'magic_links/process_authenticate', to: 'magic_links#process_authenticate', as: 'magic_links_process_authenticate'
+    post 'magic_links/process_invite', to: 'magic_links#process_invite', as: 'magic_links_process_invite'
+    post 'magic_links/process_revoke_invite', to: 'magic_links#process_revoke_invite', as: 'magic_links_process_revoke_invite'
+    post 'magic_links/process_login_or_signup', to: 'magic_links#process_login_or_signup',
+                                                as: 'magic_links_process_login_or_signup'
   end
 end
--- lib/phcdevworks_accounts_stytch/stytch/organization.rb
+++ lib/phcdevworks_accounts_stytch/stytch/organization.rb
@@ -3,23 +3,52 @@
 module PhcdevworksAccountsStytch
   module Stytch
     class Organization
-      def initialize(client: PhcdevworksAccountsStytch::Stytch::Client.b2b_client)
-        @client = client
+      def initialize
+        @client = PhcdevworksAccountsStytch::Stytch::Client.b2b_client
       end
 
       # Finds the organization by its slug and returns the organization ID
       def find_organization_id_by_slug(slug)
-        response = @client.organizations.list
-        organization = response['organizations'].find { |org| org['organization_slug'] == slug }
+        response = search_organization_by_slug(slug)
+        extract_organization_id(response, slug)
+      rescue StandardError => e
+        handle_error(e)
+      end
 
-        if organization
+      private
+
+      # Searches for the organization by slug using the Stytch API
+      def search_organization_by_slug(slug)
+        @client.organizations.search(
+          query: {
+            operator: 'OR',
+            operands: [
+              { filter_name: 'organization_slugs', filter_value: [slug] }
+            ]
+          }
+        )
+      end
+
+      # Extracts the organization ID from the response Hash
+      def extract_organization_id(response, slug)
+        organizations = response['organizations']
+        if organizations&.any?
+          organization = organizations.first
           organization['organization_id']
         else
           raise PhcdevworksAccountsStytch::Stytch::Error.new(
             status_code: 404,
-            error_message: "Organization with slug '#{slug}' not found."
+            error_message: "Organization with slug '#{slug}' not found"
           )
         end
+      end
+
+      # Handles errors raised during the process
+      def handle_error(error)
+        raise PhcdevworksAccountsStytch::Stytch::Error.new(
+          status_code: error.respond_to?(:status_code) ? error.status_code : 500,
+          error_message: error.message
+        )
       end
     end
   end
