0ae74bca321ef0c9ed23bd8341ef835240136235
BradPotts <brad.potts@coastdigitalgroup.com>
2024-09-09 23:53:01 +0800

    adding password reset views, fixing auth routes

--- config/routes/b2b.rb
+++ config/routes/b2b.rb
@@ -10,5 +10,20 @@ namespace :b2b do
   post 'magic_links/process_login_or_signup/:organization_slug', to: 'magic_links#process_login_or_signup',
                                                                  as: 'magic_links_process_login_or_signup'
 
-  post 'authenticate', to: 'authenticate#authenticate', as: 'b2b_authenticate'
+  # PasswordsController routes
+  get 'passwords/reset/start', to: 'passwords#reset_start_form', as: 'b2b_password_reset_start_form'
+  post 'passwords/reset/start', to: 'passwords#reset_start', as: 'b2b_password_reset_start'
+
+  get 'passwords/reset', to: 'passwords#reset_form', as: 'b2b_password_reset_form'
+  post 'passwords/reset', to: 'passwords#reset_process', as: 'b2b_password_reset_process'
+
+  get 'passwords/reset/existing', to: 'passwords#reset_existing_password_form', as: 'b2b_password_reset_existing_form'
+  post 'passwords/reset/existing', to: 'passwords#reset_existing_password', as: 'b2b_password_reset_existing'
+
+  get 'passwords/reset/session', to: 'passwords#reset_with_session_form', as: 'b2b_password_reset_session_form'
+  post 'passwords/reset/session', to: 'passwords#reset_with_session', as: 'b2b_password_reset_session'
+
+  # AuthenticateController route
+  get 'authenticate', to: 'authenticate#authenticate', as: 'authenticate'
+  post 'process_authenticate', to: 'authenticate#process_authenticate', as: 'process_authenticate'
 end
