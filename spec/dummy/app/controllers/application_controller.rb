fb832b4615d998f1ff19a0ccd068424c4fbd569d
BradPotts <brad.potts@coastdigitalgroup.com>
2024-10-03 09:33:25 +0800

    add CSRF protection to dummy app to squelch security warning on gh.

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  protect_from_forgery with: :exception
   allow_browser versions: :modern
 end
